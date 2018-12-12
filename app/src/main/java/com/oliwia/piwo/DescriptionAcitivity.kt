d330aeb93c6b34ffecc5247e8213de7e24fc0087
kubzdel <kubzdel@gmail.com>
2018-12-12 20:47:33 +0100

    Added pub popup, zoomed camera.

---
+++ app/src/main/java/com/oliwia/piwo/DescriptionAcitivity.kt
@@ -0,0 +1,12 @@
+package com.oliwia.piwo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DescriptionAcitivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.pub_description_view);
+    }
+}
--- app/src/main/java/com/oliwia/piwo/MainActivity.kt
+++ app/src/main/java/com/oliwia/piwo/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.oliwia.piwo
 
+import android.content.Context
+import android.content.Intent
+import android.icu.text.DecimalFormat
+import android.os.Build
 import android.os.Bundle
+import android.support.annotation.RequiresApi
 import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import android.util.Log
+import android.view.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -16,10 +21,17 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import android.widget.Toast
+import android.view.View.OnTouchListener
+import android.widget.Button
+import android.widget.PopupWindow
+import android.widget.LinearLayout
+
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    val zoomLevel = 16.0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -58,9 +70,42 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         mMap = googleMap
         /// print("hejka")
         // Add a marker in Poznan and move the camera
-        val sydney = LatLng(52.406, 16.93)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val psychodela = LatLng(52.408210, 16.935618)
+        mMap.addMarker(MarkerOptions().position(psychodela).title("Psychodela"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(psychodela,zoomLevel))
+        mMap.setOnMarkerClickListener { marker ->
+          //  if (marker.title.equals("Psychodela"))
+            // if marker source is clicked
+             //   Toast.makeText(this@MainActivity, marker.title, Toast.LENGTH_SHORT).show()// display toast
+            val inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            val popupView = inflater.inflate(R.layout.pub_popup, null)
+
+            // create the popup window
+            val width = (LinearLayout.LayoutParams.WRAP_CONTENT).toInt()
+            val height = (LinearLayout.LayoutParams.WRAP_CONTENT).toInt()
+            val focusable = true // lets taps outside the popup also dismiss it
+            val popupWindow = PopupWindow(popupView,width,height ,focusable)
+
+            // show the popup window
+            // which view you pass in doesn't matter, it is only used for the window tolken
+            popupWindow.showAtLocation(window.decorView.rootView, Gravity.BOTTOM, 0, 0)
+            val infoButton = popupView.findViewById(R.id.info) as Button
+            infoButton.setOnClickListener {
+                val i = Intent(this,DescriptionAcitivity::class.java)
+                // i.putExtra("URL",this.baseURL)
+                startActivityForResult(i,10)
+            }
+
+            // dismiss the popup window when touched
+            popupView.setOnTouchListener { v, event ->
+                popupWindow.dismiss()
+                true
+            }
+
+
+            true
+        }
+
     }
 
     override fun onBackPressed() {
@@ -106,5 +151,30 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun CalculationByDistance(StartP: LatLng, EndP: LatLng): Double {
+        val Radius = 6371// radius of earth in Km
+        val lat1 = StartP.latitude
+        val lat2 = EndP.latitude
+        val lon1 = StartP.longitude
+        val lon2 = EndP.longitude
+        val dLat = Math.toRadians(lat2 - lat1)
+        val dLon = Math.toRadians(lon2 - lon1)
+        val a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + (Math.cos(Math.toRadians(lat1))
+                * Math.cos(Math.toRadians(lat2)) * Math.sin(dLon / 2)
+                * Math.sin(dLon / 2))
+        val c = 2 * Math.asin(Math.sqrt(a))
+        val valueResult = Radius * c
+        val km = valueResult / 1
+        val newFormat = DecimalFormat("####")
+        val kmInDec = Integer.valueOf(newFormat.format(km))
+        val meter = valueResult % 1000
+        val meterInDec = Integer.valueOf(newFormat.format(meter))
+        Log.i("Radius Value", "" + valueResult + "   KM  " + kmInDec
+                + " Meter   " + meterInDec)
+
+        return Radius * c
     }
 }
