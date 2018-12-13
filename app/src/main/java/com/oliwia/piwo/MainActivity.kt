b3a50b202e9af1462905b68451d468a92a1cbd6f
Jakub Tomczak <jakub.tomczak@live.com>
2018-12-13 14:01:37 +0100

    Change lookup text view.

--- app/src/main/java/com/oliwia/piwo/MainActivity.kt
+++ app/src/main/java/com/oliwia/piwo/MainActivity.kt
@@ -98,14 +98,13 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
             }
 
-            override fun onTextChanged(enteredText: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                enteredText?.let {
-                    openLookupActivity(it)
-                }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
             }
 
             override fun afterTextChanged(p0: Editable?) {
-
+                p0?.let {
+                    openLookupActivity(it)
+                }
             }
 
         })
